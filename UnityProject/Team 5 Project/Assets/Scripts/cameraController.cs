7990ca5c6b7550a6bbc2ab735b5f18e7699346ad
DoctorJDWT-FS <158120896+DoctorJDWT-FS@users.noreply.github.com>
2024-08-12 16:05:03 -0600

    Added Player Prefab
    The player can walk/sprint/jump/and I think shoot.

---
+++ UnityProject/Team 5 Project/Assets/Scripts/cameraController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    [SerializeField] int sens;
+    [SerializeField] int lockVertMin, lockVertMax;
+    [SerializeField] bool invertY;
+
+    float rotX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get input
+        float mouseY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
+
+        if (invertY)
+            rotX += mouseY;
+        else
+            rotX -= mouseY;
+
+        //clamp the rotx on the x-axis
+        rotX = Mathf.Clamp(rotX, lockVertMin, lockVertMax);
+
+        //rotate the cam on the x-axis
+        transform.localRotation = Quaternion.Euler(rotX, 0, 0);
+
+        //rotate the PLAYER on the y-axis
+        transform.parent.Rotate(Vector3.up * mouseX);
+    }
+}
