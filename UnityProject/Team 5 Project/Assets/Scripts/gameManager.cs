c5ddaf69caf3c1d843a87b194ce7e072c4cbd944
Jason <jason.fajardo1@gmail.com>
2024-08-12 17:33:29 -0400

    Fix game from crashing

---
+++ UnityProject/Team 5 Project/Assets/Scripts/gameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+
+    int enemyCount;
+    //player info
+    public GameObject player;
+    //public playerController playerScript;
+    void Awake()
+    { 
+        instance = this;
+        player = GameObject.FindWithTag("Player");
+       // playerScript = player.GetComponent<playerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateGameGoal(int amount)
+    {
+        enemyCount += amount;
+    }
+}
