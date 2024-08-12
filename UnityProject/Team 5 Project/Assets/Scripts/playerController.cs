ed6892210b104900095c7efb403d133532ea3848
DoctorJDWT-FS <158120896+DoctorJDWT-FS@users.noreply.github.com>
2024-08-12 01:47:19 -0600

    Added a test player
    Added camera control and player control. Forgot to save him as a prefab, but you can pull him from my scene if needed.

---
+++ UnityProject/Team 5 Project/Assets/Scripts/playerController.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.EditorCoroutines.Editor;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    //[SerializeField] LayerMask ignoreMask;
+
+    //[SerializeField] int HP;
+    [SerializeField] int speed;
+    //[SerializeField] int sprintMod;
+    //[SerializeField] int jumpMax;
+    //[SerializeField] int jumpSpeed;
+    //[SerializeField] int gravity;
+
+    //[SerializeField] int shootDamage;
+    //[SerializeField] float shootRate;
+    //[SerializeField] int shootDist;
+
+    Vector3 move;
+    //Vector3 playerVel;
+
+    //int jumpCount;
+    //int HPOrig;
+
+    //bool isSprinting;
+    //bool isShooting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //HPOrig = HP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (!gameManager.instance.isPaused)
+        //{
+        //    movement();
+        //}
+
+        //sprint();
+
+
+        //move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //controller.Move(move * speed * Time.deltaTime);
+
+        move = Input.GetAxis("Vertical") * transform.forward +
+               Input.GetAxis("Horizontal") * transform.right;
+        controller.Move(move * speed * Time.deltaTime);
+
+    }
+
+    //void movement()
+    //{
+    //    if (controller.isGrounded)
+    //    {
+    //        jumpCount = 0;
+    //        playerVel = Vector3.zero;
+    //    }
+
+
+
+
+    //    if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+    //    {
+    //        jumpCount++;
+    //        playerVel.y = jumpSpeed;
+    //    }
+
+    //    controller.Move(playerVel * Time.deltaTime);
+    //    playerVel.y -= gravity * Time.deltaTime;
+
+    //if (Input.GetButton("Shoot") && !isShooting)
+    //    StartCoroutine(shoot());
+    //}
+
+    //void sprint()
+    //{
+    //    if (Input.GetButtonDown("Sprint"))
+    //    {
+    //        speed *= sprintMod;
+    //        isSprinting = true;
+    //    }
+    //    else if (Input.GetButtonUp("Sprint"))
+    //    {
+    //        speed /= sprintMod;
+    //        isSprinting = false;
+    //    }
+    //}
+
+    //IEnumerator shoot()
+    //{
+    //    isShooting = true;
+
+    //    RaycastHit hit;
+    //    if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist, ~ignoreMask))
+    //    {
+    //        //Debug.Log(hit.collider.name);
+    //        IDamage dmg = hit.collider.GetComponent<IDamage>();
+
+    //        if (dmg != null)
+    //        {
+    //            dmg.takeDamage(shootDamage);
+    //        }
+    //    }
+    //    yield return new WaitForSeconds(shootRate);
+    //    isShooting = false;
+    //}
+
+    //public void takeDamage(int amount)
+    //{
+    //    HP -= amount;
+
+    //    // I'm dead!
+    //    if (HP <= 0)
+    //    {
+    //        gameManager.instance.youLose();
+    //    }
+    //}
+}
