68f94977ed7967da2b8ce2eee9c8d37f0ead0468
DoctorJDWT-FS <158120896+DoctorJDWT-FS@users.noreply.github.com>
2024-08-16 16:27:06 -0600

    Currency scrip wrote

---
+++ UnityProject/Team 5 Project/Assets/Scripts/currencyManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class currencyManager : MonoBehaviour
+{
+    [SerializeField] int startingCurrency;
+
+    public int currentCurrency;
+
+    // Event for updating the UI or notifying other systems
+    public delegate void OnCurrencyChanged(int newAmount);
+    public event OnCurrencyChanged currencyChangedEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentCurrency = startingCurrency;
+        NotifyCurrencyChanged();
+    }
+
+    public void AddCurrency(int amount)
+    {
+        currentCurrency += amount;
+        NotifyCurrencyChanged();
+    }
+
+    public bool SpendCurrency(int amount)
+    {
+        if (currentCurrency >= amount)
+        {
+            currentCurrency -= amount;
+            NotifyCurrencyChanged();
+            return true; // Transaction successful
+        }
+        else
+        {
+            return false; // Not enough currency
+        }
+    }
+
+    public int GetCurrentCurrency()
+    {
+        return currentCurrency;
+    }
+
+    // Notify UI or other systems that the currency has changed
+    private void NotifyCurrencyChanged()
+    {
+        if (currencyChangedEvent != null)
+        {
+            currencyChangedEvent(currentCurrency);
+        }
+    }
+}
