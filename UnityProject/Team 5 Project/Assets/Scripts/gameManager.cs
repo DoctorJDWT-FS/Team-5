47294b6b1ba14c992a43f8640e98f16fdd02e993
Bella Hansen <Sharkskin56@gmail.com>
2024-09-14 11:47:43 -0400

    Added objectives to the UI and gamemanager

--- UnityProject/Team 5 Project/Assets/Scripts/gameManager.cs
+++ UnityProject/Team 5 Project/Assets/Scripts/gameManager.cs
@@ -45,6 +45,7 @@ public class gameManager : MonoBehaviour
     public Image PlayerHPBar;
     public Image playerShieldBar;
     public Image playerCreditCount;
+    public TMP_Text currentObjective;
 
     public bool invertY;
     public bool isPaused;
@@ -116,7 +117,7 @@ public class gameManager : MonoBehaviour
     public void updateGameGoal(int amount)
     {
         enemyCount += amount; 
-        // enemyCountText.text = enemyCount.ToString("F0");
+        enemyCountText.text = enemyCount.ToString("F0");
         if (enemyCount <= 0)
         {
             // temp stuff if we do wave based if not then its not temp
