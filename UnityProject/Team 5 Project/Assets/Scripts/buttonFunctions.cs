5e23e52d9c235426f37f7af24db823472814b86e
FultonEric <137821133+FultonEric@users.noreply.github.com>
2024-09-22 16:43:27 -0400

    keybindings added. needs further troubleshooting.

--- UnityProject/Team 5 Project/Assets/Scripts/buttonFunctions.cs
+++ UnityProject/Team 5 Project/Assets/Scripts/buttonFunctions.cs
@@ -82,6 +82,11 @@ public class buttonFunctions : MonoBehaviour
             gameManager.instance.openSettingsMenu();
     }
 
+    public void SaveKeybindings()
+    {
+        gameManager.instance.KBmManager.SaveKeys();
+    }
+
     public void ChangeKeybindings()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
